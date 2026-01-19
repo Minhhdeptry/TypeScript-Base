2839688ec549011334a57fab711d226b2f0a9591
Minhdeptry <minhhdeptry@gmail.com>
2026-01-19 19:44:07 +0700

    xong lab 5 - add courses

--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -1,44 +1,84 @@
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
 function AddPage() {
+  const { register, handleSubmit, formState: {errors} } = useForm();
+  const nav = useNavigate();
+  const onSubmit = async (values: any) => {
+    console.log(values);
+
+    try {
+      await axios.post(`http://localhost:3000/Courses`, values);
+      alert("Thêm mới thành công")
+      nav('/list')
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold mb-6">Thêm mới</h1>
 
-      <form className="space-y-6">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
         {/* Name input */}
         <div>
           <label htmlFor="name" className="block font-medium mb-1">
             Name
           </label>
           <input
+            {...register("name", {
+              required: "Không được để trống tên khóa học",
+              minLength: {
+                value: 3,
+                message: "Không được nhỏ hơn 3 ký tự"
+              },
+            })}
             type="text"
             id="name"
             className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
+          {errors.name && <span className="text-red-500">{errors.name.message as string}</span>}
         </div>
         <div>
           <label htmlFor="teacher" className="block font-medium mb-1">
             Teacher
           </label>
           <input
+            {...register("teacher", 
+              {
+                required: "Không được để trống giảng viên",
+                minLength: {
+                  value: 3,
+                  message: "Không được nhỏ hơn 3 ký tự"
+                },
+              }
+            )}
             type="text"
             id="teacher"
             className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
+            />
+            {errors.teacher && <span className='text-red-500'>{errors.teacher.message as string}</span>}
         </div>
 
         {/* Select */}
         <div>
-          <label htmlFor="selectOption" className="block font-medium mb-1">
-            Select - option
+          <label htmlFor="category" className="block font-medium mb-1">
+            Category
           </label>
           <select
-            id="selectOption"
+            {...register("category", {
+              required: "Cần chọn danh mục"
+            })}
+            id="category"
             className="w-full border rounded-lg px-3 py-2 bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
+            <option></option>
             <option value="Chuyên ngành">Chuyên ngành</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
+            <option value="Cơ sở">Cơ sở</option>
+            <option value="Đại cương">Đại cương</option>
           </select>
+          {errors.category && <span className="text-red-500">{errors.category.message as string}</span>}
         </div>
 
         <div>
@@ -46,10 +86,19 @@ function AddPage() {
             Credit
           </label>
           <input
-            type="text"
+            {...register("credit", {
+              valueAsNumber: true,
+              required: "Không được để trống credit",
+              min: {
+                value: 0,
+                message: "Không được nhỏ hơn 0"
+              }
+            })}
+            type="number"
             id="credit"
             className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
+          {errors.credit && <span className="text-red-500">{errors.credit.message as string}</span>}
         </div>
 
         {/* Submit button */}
